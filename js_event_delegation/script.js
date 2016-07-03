c156329ce8fe10526047858c6055e2939fd049ca
emalfano <emalfano1@gmail.com>
2016-07-03 15:46:24 -0700

    js event delegation - Elizabeth Alfano

--- js_event_delegation/script.js
+++ js_event_delegation/script.js
@@ -1,10 +1,27 @@
 // Answer the questions here:
 
 // - What do you think is going to happen?
+// - "Delegated Button #5 handler" will log to console
 
 // - What happened?
-
+// - "Delegated Button #5 handler" will log to console
 // - Why?
-
+// - Because the button was added to the #list children and the event was handled by the same handler as the first 4 children
 //========== Write your code below ===========//
+//Feature Set 1 - The lonely event listener
+$(document).ready(function () {
+    $("#list").on("click", 'button', function () {      //targets the list id buttons
+        console.log($(this).text());                    //logs out the button text node that was clicked
+    });
 
+    //Feature Set 2 - bring a friend
+    var  delegatedButton = $("<button>").css("margin", "10px").css("background-color", "lavender").text("Delegated Button #5 handler");
+    var li = $('<li>');
+    var ul = $('#list');
+    li.append(delegatedButton);
+    ul.append(li);
+
+    //Feature Set 3 - Now it's a party!
+
+
+});
