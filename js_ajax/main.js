ee79558139c84536679471c6ab3b056740671e2c
emalfano <emalfano1@gmail.com>
2016-07-19 18:52:16 -0700

    finished feature set 2

--- js_ajax/main.js
+++ js_ajax/main.js
@@ -10,8 +10,15 @@ $(document).ready(function(){
             success: function(result) {
                 console.log('AJAX Success function called, with the following result:', result);
                 global_result = result;
+                //Feature Set 2 - Exploring for images
+                //variable to hold the 3rd image of the first movie
+                var first_movie_info = global_result.feed.entry[0]['im:image'][2].label;
+
+                console.log('first_movie_info : ', first_movie_info);
             }
         });
         console.log('End of click function');
+        //feature set 2 - exploring for imges
+
     });
 });
