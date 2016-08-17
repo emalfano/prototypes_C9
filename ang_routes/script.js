1dbcc883583864436616c874d6cdcc890d0a8d00
emalfano <emalfano1@gmail.com>
2016-08-17 15:39:03 -0700

    Angular Routes - Elizabeth Alfano

--- ang_routes/script.js
+++ ang_routes/script.js
@@ -1,17 +1,31 @@
 // Create the route module and name it routeApp
 var app = angular.module("routeApp",['ngRoute']);
 // Config the routes
-
+app.config(function($routeProvider){
+    $routeProvider
          // route for the home page
-
+        .when('/',{
+            templateUrl : 'pages/home.html',
+            controller: 'routeCtrl'
+    })
         // route for the about page
-
+        .when('/contact',{
+            templateUrl : 'pages/contact.html',
+            controller: 'contactCtrl'
+    })
         // route for the contact page
-
-
+        .when('/about',{
+            templateUrl : 'pages/about.html',
+            controller: 'aboutCtrl'
+        });
+});
 // Create the controllers for the different pages below
 app.controller('routeCtrl', function($scope){
    $scope.message = "lorem ipsem";
+}).controller('contactCtrl', function($scope){
+    $scope.message = "contact lorem ipsem";
+}).controller('aboutCtrl', function($scope){
+    $scope.message = "about lorem ipsem";
 });
 // home page controller
     // Create a message to display in the view
