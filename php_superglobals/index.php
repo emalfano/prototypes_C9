80cc1fae33e22365a6d95d758c3d62e453b4c727
emalfano <emalfano1@gmail.com>
2016-08-18 15:10:28 -0700

    finished feature set 2

---
+++ php_superglobals/index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+    <title>PHP Superglobals</title>
+<!--    <script src="main.js"></script>-->
+    <script>
+
+    </script>
+</head>
+<body>
+<h1>Super Globals!</h1>
+
+<pre>
+<?php
+//var_dump($_SERVER);
+print_r($_GET);
+var_dump($_POST);
+?>
+</pre>
+
+</body>
+</html>
