1ca87fdb5ae25dcd518fff6d57e6091237cdbb10
emalfano <emalfano1@gmail.com>
2016-08-18 10:45:52 -0700

    PHP Includes - Elizabeth Alfano

---
+++ php_includes/includes/main.php
@@ -0,0 +1,4 @@
+<p>No one would have believed in the last years of the nineteenth century that this world was being watched keenly and closely by intelligences greater than man's and yet as mortal as his own; that as men busied themselves about their various concerns they were scrutinised and studied, perhaps almost as narrowly as a man with a microscope might scrutinise the transient creatures that swarm and multiply in a drop of water.</p>
+
+<img   src="http://www.authorama.com/files/the-war-of-the-worlds/cover.jpg">
+    
---
+++ php_includes/index_main.php
@@ -0,0 +1,7 @@
+<main id="main_content">
+    <?php include('includes/main.php'); ?>
+</main>
+<div>I'm not in the main content or the footer</div>
+<footer>
+    <?php include('includes/footer.php'); ?>
+</footer>
