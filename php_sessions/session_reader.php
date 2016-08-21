5c620f14e9a8df556f80cc75fe20483a52f21684
emalfano <emalfano1@gmail.com>
2016-08-21 12:49:34 -0700

    added validation for input fields

--- php_sessions/session_reader.php
+++ php_sessions/session_reader.php
@@ -1,1 +1,31 @@
 <!-- Session Reader -->
+<?php
+    session_start();
+    if(!empty($_SESSION['errors']['name']) ||
+        !empty($_SESSION['errors']['age']) ||
+        !empty($_SESSION['errors']['occupation'])){
+       unset($_SESSION['errors']);
+    }
+
+    if (preg_match('/[0-9]/', $_GET['name'],$match)){
+        $_SESSION['errors']['name'] = 'Name is invalid';
+        header('location: session_setter.php');
+    }
+    if ($_GET['age'] > 200 || preg_match('/\D/', $_GET['age'],$match)){
+        $_SESSION['errors']['age'] = 'Age is invalid';
+        header('location: session_setter.php');
+    }
+    if (preg_match('/^[0-9]/', $_GET['occupation'],$match)){
+        $_SESSION['errors']['occupation'] = 'Occupation is invalid';
+        header('location: session_setter.php');
+    }
+   
+    $_SESSION['name'] = $_GET['name'];
+    $_SESSION['age'] = $_GET['age'];
+    $_SESSION['occupation'] = $_GET['occupation'];
+
+    print('Name ' . $_SESSION['name'] . '<br>');
+    print('Age ' . $_SESSION['age']. '<br>');
+    print('Occupation ' . $_SESSION['occupation']. '<br>');
+?>
+
--- php_sessions/session_setter.php
+++ php_sessions/session_setter.php
@@ -1,1 +1,42 @@
 <!-- Session Setter -->
+<?php
+session_start();
+
+if(!empty($_SESSION['name'])) {
+    $name = $_SESSION['name'];
+} else {
+    $name = '';
+}
+if(!empty($_SESSION['age'])) {
+    $age = $_SESSION['age'];
+} else {
+    $age = '';
+}
+if(!empty($_SESSION['occupation'])) {
+    $occupation = $_SESSION['occupation'];
+} else {
+    $occupation = '';
+}
+
+?>
+<form action="session_reader.php">
+    <input name="name" value="<?=$name?>" placeholder="Name"><br>
+    <?php
+    if(!empty($_SESSION['errors']['name'])){
+            print_r($_SESSION['errors']['name'] . '<br>');
+    }
+    ?>
+    <input name="age" value="<?=$age?>" placeholder="Age"><br>
+    <?php
+    if(!empty($_SESSION['errors']['age'])){
+        print_r($_SESSION['errors']['age'] . '<br>');
+    }
+    ?>
+    <input name="occupation" value="<?=$occupation?>" placeholder="Occupation"><br>
+    <?php
+    if(!empty($_SESSION['errors']['occupation'])){
+        print_r($_SESSION['errors']['occupation'] . '<br>');
+    }
+    ?>
+    <button>Submit</button>
+</form>
