850375e9767a71320c84d794139cb2bfd72d460d
emalfano <emalfano1@gmail.com>
2016-08-22 18:11:25 -0700

    initial commit

---
+++ php_mysql/form_standard.php
@@ -0,0 +1,8 @@
+<form action="index_insert.php" method="post">
+    <input name="user" type="text" placeholder="User id">
+    <input name="title" type="text" placeholder="Title">
+    <input name="details" type="text" placeholder="Details">
+    <input name="date" type="date">
+    <input name="notes" type="text" placeholder="Notes">
+    <input type="submit">
+</form>
--- php_mysql/index_insert.php
+++ php_mysql/index_insert.php
@@ -1,1 +1,25 @@
 <!-- index_insert.php -->
+<?php
+require_once('mysql_connect.php');
+print_r($_POST['user']);
+$user = $_POST['user'];
+print_r($_POST['title']);
+$title = $_POST['title'];
+print_r($_POST['details']);
+$details = $_POST['details'];
+print_r($_POST['date']);
+$date = $_POST['date'];
+print_r($_POST['notes']);
+$query = "INSERT INTO `todo_items` (`title`, `details`, `user_id`, `timestamp`)
+      VALUES ('$title', '$details', '$user', '$date')";
+$result = mysqli_query($conn, $query);
+print('affected rows '.mysqli_affected_rows($conn));
+if(mysqli_affected_rows($conn) > 0) {
+    print('<br>table updated!');
+}elseif (mysqli_affected_rows($conn) == -1){
+    print('bad query : '. mysqli_error($conn));
+} else {
+    print('<br>table not updated! try again');
+}
+?>
+
