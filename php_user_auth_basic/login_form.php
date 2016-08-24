f18235fbb571e44ec34d3657e5521c98842ee2a0
emalfano <emalfano1@gmail.com>
2016-08-24 16:58:21 -0700

    finished feature set 1

--- php_user_auth_basic/login_form.php
+++ php_user_auth_basic/login_form.php
@@ -1,1 +1,42 @@
 <!--login_form.php-->
+<!doctype html>
+<html>
+<head>
+    <script src="https://code.jquery.com/jquery-2.1.4.js"></script>
+</head>
+<body>
+<?php
+session_start();
+
+if(!empty($_SESSION['message'])){
+    $message = $_SESSION['message'];
+} else {
+    $message = '';
+}
+?>
+Login: <input name="username" id="username" placeholder="Username">
+<input name="password" id="password" placeholder="Password">
+<input type="submit" value="Submit" onclick="loginHandler()" >
+<p id="message"><?=$message?></p>
+
+<script>
+    var loginHandler = function(){
+        console.log('the function loginHandler has been called');
+        $.ajax({
+            url: "login_handler.php",
+            method: "POST",
+            data: {'username': $('#username').val(), 'password': $('#password').val()},
+            dataType:'text',
+            cache: false,
+            success: function(response){
+                console.log('Call success ', response);
+            },
+            error: function(response){
+                console.log('Call fail ', response);
+            }
+        });
+
+    };
+</script>
+</body>
+</html>
--- php_user_auth_basic/login_handler.php
+++ php_user_auth_basic/login_handler.php
@@ -1,1 +1,31 @@
 <!--login_handler.php-->
+<?php
+session_start();
+$user_info = [
+    ['id'=> 1, 'username'=>'beth', 'password' => 'fluffybunnies'],
+    ['id'=> 2, 'username'=>'meg', 'password' => 'shakeandbake'],
+    ['id'=> 3, 'username'=>'jo', 'password' => '12345'],
+    ['id'=> 4, 'username'=>'amy', 'password' => 'supergoodpassword'],
+    ['id'=> 5, 'username'=>'professor bear', 'password' => 'ilovejo']
+];
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+foreach ($user_info as $user){  // loop through users
+    if($username == $user['username']){ // found user
+        print('found user');
+        print_r($user);
+        if ($password == $user['password']){ // does password match?
+            print('password matches');
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['message'] = 'You are now logged in!';
+            break;
+        }
+    } else {
+        continue;
+    }
+    $_SESSION['message'] = 'Your username OR password is incorrect!';
+    print('Your username or pw is incorrect');
+}
+?>
